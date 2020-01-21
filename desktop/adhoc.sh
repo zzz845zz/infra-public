88e68db170582c587d60e8b7aa278e8bce32182e
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2020-01-22 01:35:22 +0900

    Add vscode

--- desktop/adhoc.sh
+++ desktop/adhoc.sh
@@ -2,3 +2,5 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+
+sudo snap install --classic code
