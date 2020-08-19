4cba5799616a9a626ad43cc56ec135a5d55bb3e3
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-08-19 11:18:54 +0900

    add nimf

--- desktop/adhoc.sh
+++ desktop/adhoc.sh
@@ -3,4 +3,9 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 
+# nimf. After installing, run `im-config -n nimf` and login again.
+wget -O - http://apt.hamonikr.org/hamonikr.key | sudo apt-key add -
+sudo bash -c "echo 'deb https://apt.hamonikr.org jin main upstream' > /etc/apt/sources.list.d/hamonikr-jin.list"
+sudo bash -c "echo 'deb-src https://apt.hamonikr.org jin main upstream' >> /etc/apt/sources.list.d/hamonikr-jin.list"
+
 sudo snap install --classic code
