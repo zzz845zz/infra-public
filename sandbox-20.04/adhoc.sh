3207b3c2bb02c2cb0d47a50925239d87bff62371
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2021-02-12 23:46:43 +0900

    Update adhoc.sh

--- sandbox-20.04/adhoc.sh
+++ sandbox-20.04/adhoc.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
+
+curl "http://10.21.0.3:3142/acng-report.html?abortOnErrors=aOe&byPath=bP&byChecksum=bS&truncNow=tN&incomAsDamaged=iad&purgeNow=pN&doExpire=Start+Scan+and%2For+Expiration&calcSize=cs&asNeeded=an#bottom"
