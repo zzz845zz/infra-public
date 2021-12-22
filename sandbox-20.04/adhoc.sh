b2cb5d1cbd624f234a06c0a76fb2e4d458bd19ac
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2021-12-23 01:13:19 +0900

    Support scala

--- sandbox-20.04/adhoc.sh
+++ sandbox-20.04/adhoc.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+# vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
+# hamonikr ime
 curl -sL https://pkg.hamonikr.org/add-hamonikr.apt | sudo -E bash -
+
+# sbt
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
