9e9d96fdc5658c7467e7dffac51384b1807ba72b
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2022-01-26 09:03:11 +0900

    Update path

--- sandbox/20.04-x86_64-adhoc.sh
+++ sandbox/20.04-x86_64-adhoc.sh
@@ -7,9 +7,6 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
-# hamonikr ime
-curl -sL https://pkg.hamonikr.org/add-hamonikr.apt | sudo -E bash -
-
 # sbt
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
